37bf1bbaf453e0459ed5498b9552b1ab0e01af96
Silviu Bocsa <silviubocsa@gmail.com>
2021-07-29 15:19:16 +0300

    sad

--- src/App.js
+++ src/App.js
@@ -22,7 +22,12 @@ function App() {
       <GlobalStyles></GlobalStyles>
       <StyledApp>
         <Layout>
-
+          <div>
+            <label className={classes.switch}>
+              <input type="checkbox"onClick={() => themeToggler()}></input>
+              <span className={classes.slider}></span>
+            </label>
+          </div>
 
           <Switch>
             <Route path="/" exact></Route>
